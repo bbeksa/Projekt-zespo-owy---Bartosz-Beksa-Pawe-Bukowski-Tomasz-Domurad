1c61a31866d1aea555c1df24ab4e90d2ceae281c
bbeksa <58745241+bbeksa@users.noreply.github.com>
2021-11-08 16:48:32 +0100

    Add files via upload

---
+++ projektZespolowy/main.py
@@ -0,0 +1,19 @@
+# adds more image processing capabilities
+import pytesseract as pytesseract
+from PIL import Image, ImageEnhance
+
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+img = Image.open('img/przykladowa-faktura-pdf.jpg')
+
+#wyostrzenie i zwiększenie kontrastu
+enhancer1 = ImageEnhance.Sharpness(img)
+enhancer2 = ImageEnhance.Contrast(img)
+img_edit = enhancer1.enhance(20.0)
+img_edit = enhancer2.enhance(1.5)
+img_edit.save("edited_image.png")
+
+result = pytesseract.image_to_string(img_edit)
+with open('text_result2.txt', mode ='w') as file:
+ file.write(result)
+ print("ready")
