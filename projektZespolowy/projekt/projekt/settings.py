fdbc4496621ba3a3f0f2b4cac415187b760b7830
Tomasz Domurad <tdomurad@protonmail.com>
2022-01-17 03:32:41 +0100

    gitingore and requirements

--- projektZespolowy/projekt/projekt/settings.py
+++ projektZespolowy/projekt/projekt/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd=b2r+s$4pc8a0m195r_99!umy8*3vesv=2klqwg*3mqr-c9sc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
